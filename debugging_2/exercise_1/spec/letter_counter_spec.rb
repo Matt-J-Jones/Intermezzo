0bd1c1c11b13c2d72f2bf8f80518bc8da2a5cd99
Matt Jones <67526111+Matt-J-Jones@users.noreply.github.com>
2023-03-17 11:39:14 +0000

    Completed debugging 2 challenge

--- debugging_2/exercise_1/spec/letter_counter_spec.rb
+++ debugging_2/exercise_1/spec/letter_counter_spec.rb
@@ -5,3 +5,17 @@ require 'letter_counter'
 # p counter.calculate_most_common
 # Intended output:
 # [2, "i"]
+
+RSpec.describe LetterCounter do
+  it "counts letters in string 'baaa' and returns most common letter & count" do
+    counter = LetterCounter.new("baaa")
+    result = counter.calculate_most_common
+    expect(result).to eq [3, "a"]
+  end
+
+  it "counts letters in string 'Digital Punk' and returns most common letter & count" do
+    counter = LetterCounter.new("Digital Punk")
+    result = counter.calculate_most_common
+    expect(result).to eq [2, "i"]
+  end
+end
